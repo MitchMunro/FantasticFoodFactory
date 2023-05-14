e7b9d3081f0f90ec70b2a0633cd215dbbccce1fe
MitchMunro <mitchmunro@gmail.com>
2023-05-15 07:11:03 +1000

    A bunch of changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,34 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Tutorial Level_New Art");
     }
+
+    public void Level1Button()
+    {
+        SceneManager.LoadScene("");
+
+    }
+
+    public void Level2Button()
+    {
+        SceneManager.LoadScene("");
+
+    }
+
+    public void Level3Button()
+    {
+        SceneManager.LoadScene("Level 3 - Meatball Pinball - Mitch");
+
+    }
+
+    public void Level4Button()
+    {
+        SceneManager.LoadScene("");
+
+    }
+
+    public void Level5Button()
+    {
+        SceneManager.LoadScene("");
+
+    }
 }
